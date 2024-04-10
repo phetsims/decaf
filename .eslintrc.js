96b25a8246ee86e9e20bef57258fec5f3e545473
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-04-10 17:05:13 -0600

    ignore three repos for linting,  https://github.com/phetsims/chipper/issues/1429
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// Copyright 2024, University of Colorado Boulder
+// @author Michael Kauzmann
+
+module.exports = {
+  extends: '../chipper/eslint/phet-library_eslintrc.js',
+  ignorePatterns: [
+    '**/**'
+  ]
+};
