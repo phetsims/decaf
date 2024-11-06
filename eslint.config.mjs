8168db4615abd78d7d2acd28176a688bb1a99bf6
Sam Reid <reids@colorado.edu>
2024-11-06 16:02:07 -0700

    Move files to eslint/config, see https://github.com/phetsims/perennial/issues/399

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 // @author Michael Kauzmann
 
-import phetLibraryEslintConfig from '../perennial-alias/js/eslint/phet-library.eslint.config.mjs';
+import phetLibraryEslintConfig from '../perennial-alias/js/eslint/config/phet-library.eslint.config.mjs';
 
 export default [
   ...phetLibraryEslintConfig,
