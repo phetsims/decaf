ef50ce87ddf58f0f4b46f7c94ed8befcb7f402bf
Sam Reid <reids@colorado.edu>
2025-10-07 12:51:24 -0600

    Use strictBooleanExpressionsConfig, see https://github.com/phetsims/perennial/issues/464

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 // Copyright 2024, University of Colorado Boulder
 // @author Michael Kauzmann
 
+import strictBooleanExpressionsConfig from '../perennial-alias/js/eslint/config/util/strictBooleanExpressionsConfig.mjs';
 import phetLibraryEslintConfig from '../perennial-alias/js/eslint/config/phet-library.eslint.config.mjs';
 
 export default [
   ...phetLibraryEslintConfig,
+  ...strictBooleanExpressionsConfig,
   {
     ignores: [
       '**/**'
